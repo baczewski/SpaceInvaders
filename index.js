2c23975fa1bd500de40ec004ead750fbfbd99830
Мартин Маринов <trafficker.shadia@gmail.com>
2023-04-26 20:34:43 +0300

    Create & move player

--- index.js
+++ index.js
@@ -3,3 +3,115 @@ const context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+class Player {
+    constructor() {
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
+
+        this.rotation = 0;
+
+        const image = new Image();
+        image.src = './assets/spaceship.png';
+        image.onload = () => {
+            const scale = 0.15;
+
+            this.image = image;
+            this.width = image.width * scale;
+            this.height = image.height * scale;
+
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height - this.height - 20
+            };
+        }
+    }
+
+    draw() {
+        context.save();
+        context.translate(
+            player.position.x + player.width / 2, 
+            player.position.y + player.height / 2
+        );
+
+        context.rotate(this.rotation);
+
+        context.translate(
+            -(player.position.x + player.width / 2), 
+            -(player.position.y + player.height / 2)
+        );
+
+        context.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);    
+ 
+        context.restore();
+    }
+
+    update() {
+        if (this.image) {
+            this.draw();
+            this.position.x += this.velocity.x;
+        }
+    }
+}
+
+const keys = {
+    a: { 
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+};
+
+const player = new Player();
+
+function animate() {
+    requestAnimationFrame(animate);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+
+    player.velocity.x = 0;
+    player.rotation = 0;
+
+    if (keys.a.pressed && player.position.x >= 0) {
+        player.velocity.x = -5;
+        player.rotation = -0.15;
+    } else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+        player.velocity.x = 5;
+        player.rotation = 0.15;
+    }
+}
+
+animate();
+
+addEventListener('keydown', ({ key }) => {
+    switch (key) {
+        case 'a':
+            keys.a.pressed = true;
+            break;
+        case 'd':
+            keys.d.pressed = true;
+            break;
+        case ' ':
+            break;
+    }
+});
+
+addEventListener('keyup', ({ key }) => {
+    switch (key) {
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case ' ':
+            break;
+    }
+});
